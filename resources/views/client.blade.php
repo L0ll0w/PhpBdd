ea834401d2ff24fd208374438a908abb7709b2d9
L0ll0w <matheo.beaudet@edu.esiee-it.fr>
2025-02-21 11:59:13 +0100

    vf

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Client</title>
+    <title>Client - Espace de réservation</title>
+    <link rel="stylesheet" href="{{ asset('css/client.css') }}">
 </head>
 
 <body>
-    <h1>Bienvenue Client</h1>
-    <a href="{{ route('schedules') }}">Voir les horaires</a>
+<div class="container">
+    <h1>Bienvenue dans votre espace client</h1>
+    <p>Consultez et réservez vos horaires en toute simplicité.</p>
+
+    <a href="{{ route('schedules') }}" class="btn">Voir les horaires</a>
+</div>
 </body>
+
 </html>
